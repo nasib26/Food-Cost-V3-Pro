ca06e3281f264d4aeeebbdac14765e3052bdbd9d
nasib26 <nasibsureshkumargiri@gmail.com>
2026-09-07 08:43:02 +0300

    Delete sw.js

--- sw.js
+++
@@ -1,5 +0,0 @@
-const CACHE='food-cost-pro-v4';
-const ASSETS=['./','./index.html','./manifest.json','./favicon.ico','./icons/icon-192-v3.png','./icons/icon-512-v3.png','./icons/favicon-32.png','./icons/favicon-16.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))).then(()=>self.clients.claim())));
-self.addEventListener('fetch',e=>{if(e.request.method!=='GET') return;e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request).then(x=>{const y=x.clone();caches.open(CACHE).then(c=>c.put(e.request,y));return x}).catch(()=>caches.match('./index.html'))));});
